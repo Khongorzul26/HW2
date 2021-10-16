4e7b39825356c237005eb620dc4dc99b9ee41dd2
Khongorzul <you@example.com>
2021-10-16 16:55:21 +0800

    After doing Task 1-2

--- hw2.py
+++ hw2.py
@@ -1,3 +1,9 @@
 #Task 1-1
 import pandas as pd
 df = pd.read_excel("C:/Users/khongorzul/Documents/Python/Week2/HW2/data.xlsx")
+
+#Task 1-2
+
+#25 buyu tuunees deesh nastai emegteichuudiin ners
+df=df[(df["age"]>25) & (df["gender"] != "M")]["firstName"]
+df.csv = df.to_csv(index = False)
